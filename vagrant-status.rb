179e952fcedeaf40065630d36ed501e0411f0581
Camron Schwoegler <camron@metafour.org>
2013-12-10 15:51:08 -0700

    Add colorized output

--- vagrant-status.rb
+++ vagrant-status.rb
@@ -6,6 +6,7 @@ puts "box\t\t\t\tstatus\t\t\tprovider\t\tstate"
 puts "---\t\t\t\t------\t\t\t--------\t\t-----"
 
 spacer = "                               "
+count = 0
 
 Dir.foreach(".") do |dir|
   if Dir.exists? dir
@@ -17,9 +18,15 @@ Dir.foreach(".") do |dir|
           words = line.split(' ')
           spaces = 31 - dir.length
           if spaces <= 0
+            count += 1
+            printf "\033[0;35m" if count % 2 == 0
             puts dir[0..30] + "…" + words[1] + spacer[0..(23 - words[1].length)] + words[2][1..-2] + spacer[0..(23 - words[2][1..-2].length)] + words[0]
+            printf "\033[0m"
           else
+            count += 1
+            printf "\033[0;35m" if count % 2 == 0
             puts dir + spacer[0..(31 - dir.length)] + words[1] + spacer[0..(23 - words[1].length)] + words[2][1..-2] + spacer[0..(23 - words[2][1..-2].length)] + words[0]
+            printf "\033[0m"
           end
         end
         i += 1
